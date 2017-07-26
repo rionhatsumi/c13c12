835266309c923e54531843dc1353365234ddf2ee
rionhatsumi <lolwxy@gmail.com>
2017-07-26 17:14:13 +0800

    edition 2
    add ucf to fig1

--- plot_normalize.cpp
+++ plot_normalize.cpp
@@ -29,19 +29,19 @@ void plot_normalize()
 	c1_1->SetFrameBorderMode(0);
 
 	//define TLegend
-	TLegend *leg = new TLegend(0.32, 0.62, 0.95, 0.95);
-	TLegend *leg2 = new TLegend(0.45, 0.75, 0.95, 0.95);
-	TLegend *leg3 = new TLegend(0.45, 0.65, 0.95, 0.95);
+	TLegend *leg = new TLegend(0.36, 0.72, 0.99, 0.93);
+	TLegend *leg2 = new TLegend(0.36, 0.82, 0.99, 0.95);
+	TLegend *leg3 = new TLegend(0.36, 0.69, 0.99, 0.99);
 	leg->SetNColumns(3);
 //	leg->SetFillColor(10);
 	leg->SetBorderSize(0);
-	leg->SetTextSize(0.065);
+	leg->SetTextSize(0.058);
 	leg->SetFillStyle(0);
 	leg->SetTextFont(132);
 	leg2->SetNColumns(3);
 //	leg2->SetFillColor(10);
 	leg2->SetBorderSize(0);
-	leg2->SetTextSize(0.065);
+	leg2->SetTextSize(0.052);
 	leg2->SetFillStyle(0);
 	leg2->SetTextFont(132);
 	// exp data for 12C+12C
@@ -58,13 +58,13 @@ void plot_normalize()
 	TF1* func_hind_c12c12 = new TF1("func_hind_c12c12",hindrance_s,0.5,5.0,5);
 	func_hind_c12c12->SetParameters(-1.32,52.93,3.68,2.55e16,6.0);
 	func_hind_c12c12->SetLineStyle(kDashed);
-	func_hind_c12c12->SetLineColor(kRed);
+	func_hind_c12c12->SetLineColor(kBlack);
 
 	TMultiGraph* mg_c12c12_smod=new TMultiGraph;
+	mg_c12c12_smod->Add(gr_c12c12sp, "PZ");
 	mg_c12c12_smod->Add(gr_becker, "PZ");	
 	mg_c12c12_smod->Add(gr_c12c12ko, "PZ");
 	mg_c12c12_smod->Add(gr_c12c12pa, "PZ");
-	mg_c12c12_smod->Add(gr_c12c12sp, "PZ");
 	//exp data for 12C+13C
 	//the smod calculation use the factor 87.21
 	TGraphErrors* gr_c12c13dayras=new TGraphErrors("exp_data/c12c13/stockstad.smod","%lg %lg %lg");
@@ -73,17 +73,20 @@ void plot_normalize()
 	TGraphErrors* gr_c12c13das=new TGraphErrors("exp_data/c12c13/c12c13_dasmahapatra.smod","%lg %lg %lg");
 	TGraphErrors* gr_c12c13anl=new TGraphErrors("exp_data/c12c13/c12c13_rehm_0.05err.smod","%lg %lg %lg %lg");
 	TGraphErrors* gr_c12c13imp=new TGraphErrors("exp_data/c12c13/imp_experr.smod","%lg %lg %lg");	
+	TGraphErrors* gr_c12c13pku=new TGraphErrors("exp_data/c12c13/imp_PKU_experr.smod","%lg %lg %lg");	
 	TGraphErrors* gr_c12c13void;	
 
 	convert_smod_2_s(gr_c12c13dayras,12*13/25.0);
 	convert_smod_2_s(gr_c12c13nd,12*13/25.0);
 	convert_smod_2_s(gr_c12c13das,12*13/25.);
 	convert_smod_2_s(gr_c12c13imp,12*13/25.);
+	convert_smod_2_s(gr_c12c13pku,12*13/25.);
 	TMultiGraph* mg_c12c13_smod=new TMultiGraph;
+	mg_c12c13_smod->Add(gr_c12c13das, "PZ");
 	mg_c12c13_smod->Add(gr_c12c13dayras, "PZ");
 	mg_c12c13_smod->Add(gr_c12c13nd, "PZ");
-	mg_c12c13_smod->Add(gr_c12c13das, "PZ");
 	mg_c12c13_smod->Add(gr_c12c13imp, "PZ");
+	mg_c12c13_smod->Add(gr_c12c13pku, "PZ");
 
 	TF1* func_hind_c12c13 = new TF1("func_hind_c12c13",hindrance_s,0.5,5.0,5);
 	func_hind_c12c13->SetParameters(-2.32,59.37,3.45,6.23e16,12*13/25.);
@@ -94,35 +97,42 @@ void plot_normalize()
 	{	// set colors for experimental data
 		// c12+c12
 		gr_becker->SetLineWidth(2);
-		gr_becker->SetLineColor(kMagenta);
-		gr_becker->SetMarkerColor(kMagenta);
-		gr_becker->SetMarkerStyle(24);
+		gr_becker->SetLineColor(kRed);
+		gr_becker->SetMarkerColor(kRed);
+		gr_becker->SetMarkerStyle(26);
+		gr_becker->SetMarkerSize(1.1);
 
 		gr_c12c12ko->SetLineWidth(2);
 		gr_c12c12ko->SetLineColor(1);
 		gr_c12c12ko->SetMarkerColor(1);
-		gr_c12c12ko->SetMarkerStyle(24);
+		gr_c12c12ko->SetMarkerStyle(22);
+		gr_c12c12ko->SetMarkerSize(1.3);
 
 		gr_c12c12pa->SetLineWidth(2);
 		gr_c12c12pa->SetLineColor(kGreen+2);
 		gr_c12c12pa->SetMarkerColor(kGreen+2);
-		gr_c12c12pa->SetMarkerStyle(24);	
+		gr_c12c12pa->SetMarkerStyle(22);	
+		gr_c12c12pa->SetMarkerSize(1.3);
 
 		gr_c12c12sp->SetLineWidth(2);
-		gr_c12c12sp->SetLineColor(kBlue);
-		gr_c12c12sp->SetMarkerColor(kBlue);
-		gr_c12c12sp->SetMarkerStyle(24);
+		//gr_c12c12sp->SetLineColor(kBlue+2);
+		gr_c12c12sp->SetLineColor(kGray+2);
+		gr_c12c12sp->SetMarkerColor(kGray+2);
+		gr_c12c12sp->SetMarkerStyle(22);
+		gr_c12c12sp->SetMarkerSize(1.3);
 
 		//c12+c13
 		gr_c12c13nd->SetLineWidth(2);
-		gr_c12c13nd->SetLineColor(kRed);
-		gr_c12c13nd->SetMarkerColor(kRed);
+		gr_c12c13nd->SetLineColor(kBlack);
+		gr_c12c13nd->SetMarkerColor(kBlack);
 		gr_c12c13nd->SetMarkerStyle(20);
+		gr_c12c13nd->SetMarkerSize(1.3);
 
 		gr_c12c13dayras->SetLineWidth(2);
-		gr_c12c13dayras->SetLineColor(kBlue);
-		gr_c12c13dayras->SetMarkerColor(kBlue);
+		gr_c12c13dayras->SetLineColor(kGreen+1);
+		gr_c12c13dayras->SetMarkerColor(kGreen+1);
 		gr_c12c13dayras->SetMarkerStyle(20);
+		gr_c12c13dayras->SetMarkerSize(1.4);
 
 		gr_c12c13ko->SetLineWidth(2);
 		gr_c12c13ko->SetLineColor(49);
@@ -130,9 +140,10 @@ void plot_normalize()
 		gr_c12c13ko->SetMarkerStyle(20);
 
 		gr_c12c13das->SetLineWidth(2);
-		gr_c12c13das->SetLineColor(kGreen+2);
-		gr_c12c13das->SetMarkerColor(kGreen+2);
-		gr_c12c13das->SetMarkerStyle(20);
+		gr_c12c13das->SetLineColor(kBlack);
+		gr_c12c13das->SetMarkerColor(kBlack);
+		gr_c12c13das->SetMarkerStyle(24);
+		gr_c12c13das->SetMarkerSize(1.3);
 
 		gr_c12c13anl->SetLineWidth(2);
 		gr_c12c13anl->SetLineColor(6);
@@ -140,9 +151,15 @@ void plot_normalize()
 		gr_c12c13anl->SetMarkerStyle(33);
 
 		gr_c12c13imp->SetLineWidth(2);
-		gr_c12c13imp->SetLineColor(kBlack);
-		gr_c12c13imp->SetMarkerColor(kBlack);
+		gr_c12c13imp->SetLineColor(kRed);
+		gr_c12c13imp->SetMarkerColor(kRed);
 		gr_c12c13imp->SetMarkerStyle(20);
+		gr_c12c13imp->SetMarkerSize(1.3);
+		gr_c12c13pku->SetLineWidth(2);
+		gr_c12c13pku->SetLineColor(kAzure-3);
+		gr_c12c13pku->SetMarkerColor(kAzure-3);
+		gr_c12c13pku->SetMarkerSize(1.3);
+		gr_c12c13pku->SetMarkerStyle(20);
 
 		// use plot_c12c13_exp.C to determine the normalization ratio	
 		// DASMAHAPATA/Dayras=1.11 +/- 0.07
@@ -150,6 +167,7 @@ void plot_normalize()
 		cout<<"DASMAHAPATA scaling factor : "<<1.0/1.11<<endl;
 		// IMP/nd=1.119 +/- 0.23, nd/Dayras=0.93 +/-0.10
 		scale_gr_err(gr_c12c13imp,1/1.119*1/0.93);
+		scale_gr_err(gr_c12c13pku,1/1.119*1/0.93);
 		cout<<"IMP scaling factor : "<<1.0/(0.93*1.119)<<endl;	
 		// use plot_c12c13_exp.C to determine the normalization ratio
 		// nd/Dayras=0.93 +/- 0.10	
@@ -168,26 +186,21 @@ void plot_normalize()
 	h2_sfactor->GetXaxis()->SetTitle("E_{c.m.} (MeV)");
 	h2_sfactor->GetXaxis()->CenterTitle(true);
 	h2_sfactor->GetXaxis()->SetLabelFont(132);
-	h2_sfactor->GetXaxis()->SetLabelSize(0.07);
-	h2_sfactor->GetXaxis()->SetTitleSize(0.10);
-	h2_sfactor->GetXaxis()->SetTitleOffset(0.75);
+	h2_sfactor->GetXaxis()->SetLabelSize(0.09);
+	h2_sfactor->GetXaxis()->SetTitleSize(0.08);
+	h2_sfactor->GetXaxis()->SetTitleOffset(0.99);
 	h2_sfactor->GetXaxis()->SetTitleFont(132);
 	h2_sfactor->GetYaxis()->SetTitle("S factor (MeV b)");
 	h2_sfactor->GetYaxis()->CenterTitle(true);
 	h2_sfactor->GetYaxis()->SetLabelFont(132);
-	h2_sfactor->GetYaxis()->SetLabelSize(0.07);
-	h2_sfactor->GetYaxis()->SetTitleSize(0.10);
-	h2_sfactor->GetYaxis()->SetTitleOffset(0.5);
+	h2_sfactor->GetYaxis()->SetLabelSize(0.09);
+	h2_sfactor->GetYaxis()->SetTitleSize(0.08);
+	h2_sfactor->GetYaxis()->SetTitleOffset(0.75);
 	h2_sfactor->GetYaxis()->SetTitleFont(132);
-	h2_sfactor->GetYaxis()->SetRangeUser(1e14, 5e17);
+	h2_sfactor->GetYaxis()->SetRangeUser(1e15, 3e17);
 
 	TMultiGraph* mg_c13c13_smod=new TMultiGraph;
 	mg_c13c13_smod->Add(gr_c13c13tren_smod, "PZ");
-
-	h2_sfactor->Draw();
-
-	mg_c12c12_smod->Draw("p");
-	func_hind_c12c12->Draw("same");
 
 	Double_t xmin_fit, xmax_fit;
 	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
@@ -199,10 +212,7 @@ void plot_normalize()
 	func_hind_c12c13->SetRange(0.5,xmax_fit);
 	//	func_hind_c12c13->Draw("same");
 
-	mg_c12c13_smod->Draw("P");
-	//	func_hind_c12c13->Draw("same");
-
-	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//---------------------------------------------------
 	cout<<"Fitting 13C+13C ........."<<endl<<endl;
 	xmin_fit=2.0, xmax_fit=5.0;
 	TF1* func_hind_c13c13 = new TF1("func_hind_c13c13",hindrance_s,xmin_fit,xmax_fit,5);
@@ -210,24 +220,28 @@ void plot_normalize()
 //	func_hind_c13c13->SetRange(0.5,xmax_fit);	
 
 //	mg_c13c13_smod->Draw("P");
-	//	func_hind_c13c13->Draw("same");	
+	//func_hind_c13c13->Draw("same");	
 
 	leg->AddEntry(gr_c12c12sp, "^{12}C+^{12}C(SP07)", "P");		
 	leg->AddEntry(gr_c12c12pa, "^{12}C+^{12}C(AG06)", "P");		
 	leg->AddEntry(gr_becker, "^{12}C+^{12}C(BK81)", "P");		
 //	leg->AddEntry(gr_c12c12ko, "^{12}C+^{12}C(Kovar)", "P");		
+	leg->AddEntry(gr_c12c13das, "^{12}C+^{13}C(Das)", "P");
 	leg->AddEntry(gr_c12c13dayras, "^{12}C+^{13}C(DA76)", "P");
 	leg->AddEntry(gr_c12c13nd, "^{12}C+^{13}C(NO11)", "P");
-	leg->AddEntry(gr_c12c13das, "^{12}C+^{13}C(Das)", "P");
 	leg->AddEntry(gr_c12c13void, "", "");
-	leg->AddEntry(gr_c12c13void, "", "");
+	leg->AddEntry(gr_c12c13pku, "^{12}C+^{13}C(PKU)", "P");
 	leg->AddEntry(gr_c12c13imp, "^{12}C+^{13}C(this work)", "P");
 //	leg->AddEntry(gr_c13c13tren_smod, "^{13}C+^{13}C(TR88)", "P");
+	h2_sfactor->Draw();
+	mg_c12c12_smod->Draw("p");
+	mg_c12c13_smod->Draw("P");
 	plot_m3y(leg3);
 	plot_tdhf(leg3);
 	plot_spp(leg3);
 	plot_esw(leg3);
 	plot_kns(leg3);
+	func_hind_c12c12->Draw("same");
 
 // spp from fig1
 	TGraph *ufc12c12spp = new TGraph("gasques/spp.sfactor", "%lg %lg");
@@ -236,9 +250,10 @@ void plot_normalize()
 	ufc12c12spp->SetLineStyle(kDashed);
 //	leg->AddEntry(ufc12c12spp, "gasques", "l");
 	plot_hind();
-	//	cal_hind();
 
-	//	plot_hind_mc();
+	//-----------------------------
+	//-fig(b)
+	//-----------------------------
 	leg->Draw();
 	c1->cd();
 	TPad *c1_2 = new TPad("c1_2", "c1_2", 1e-5, 0., 0.99999, 0.53);
@@ -255,17 +270,22 @@ void plot_normalize()
 	c1_2->SetFrameBorderMode(0);
 	h2_sfactor->Draw();
 	mg_c12c12_smod->Draw("p");
-	func_hind_c12c12->Draw("same");
 	mg_c12c13_smod->Draw("P");
 	plot_m3y_n(leg2);
 	plot_tdhf_n(leg2);
 	plot_spp_n(leg2);
 	plot_esw(leg2);
+	plot_esw_uf(leg2);
 	plot_kns(leg2);
 	plot_hind();
-	leg2->AddEntry(func_hind_c12c12, "hindrance", "L");
+	func_hind_c12c12->Draw("same");
+	//leg2->AddEntry(gr_c12c13void, "", "");
+	//leg2->AddEntry(gr_c12c13void, "", "");
+	leg2->AddEntry(func_hind_c12c12, "Hindrance", "L");
 	leg2->Draw();
 //	ufc12c12spp->Draw("C");
+	
+	TFile f("fig1.root","recreate"); c1->Write(); f.Close();
 
 }
 
@@ -363,9 +383,9 @@ void plot_m3y(TLegend *leg) {
 	TGraph* gr_c13c13 = new TGraph("henning/henning_c13c13_cc_modified.dat","%lg %lg");
 	convert_xsec_2_s(gr_c13c13,6.5,1e-3);	
 
-	gr_c12c12->SetLineColor(kBlue);
-	gr_c12c13->SetLineColor(kBlue);
-	gr_c13c13->SetLineColor(kBlue);
+	gr_c12c12->SetLineColor(kRed);
+	gr_c12c13->SetLineColor(kRed);
+	gr_c13c13->SetLineColor(kRed);
 
 	gr_c12c12->SetLineWidth(2);
 	gr_c12c13->SetLineWidth(2);
@@ -385,9 +405,9 @@ void plot_m3y_n(TLegend *leg) {
 	TGraph* gr_c13c13_n = new TGraph("henning/henning_c13c13_cc_modified.dat","%lg %lg");
 	convert_xsec_2_s(gr_c13c13_n,6.5,1e-3 * 0.73);	
 
-	gr_c12c12_n->SetLineColor(kBlue);
-	gr_c12c13_n->SetLineColor(kBlue);
-	gr_c13c13_n->SetLineColor(kBlue);
+	gr_c12c12_n->SetLineColor(kRed);
+	gr_c12c13_n->SetLineColor(kRed);
+	gr_c13c13_n->SetLineColor(kRed);
 
 	gr_c12c12_n->SetLineWidth(2);
 	gr_c12c13_n->SetLineWidth(2);
@@ -397,7 +417,7 @@ void plot_m3y_n(TLegend *leg) {
 	gr_c12c13_n->Draw("C");
 //	gr_c13c13_n->Draw("C");
 //	gr_c12c12_n->Print("all");
-	leg->AddEntry(gr_c12c12_n, "M3Y+Rep", "L");
+	leg->AddEntry(gr_c12c12_n, "CC-M3Y+Rep", "L");
 }
 
 void plot_spp(TLegend *leg) {
@@ -409,8 +429,11 @@ void plot_spp(TLegend *leg) {
 	convert_xsec_2_s(gr_c13c13,6.5, 1);	
 
 	gr_c12c12->SetLineColor(kRed);
-	gr_c12c13->SetLineColor(kRed);
+	gr_c12c13->SetLineColor(kAzure-3);
 	gr_c13c13->SetLineColor(kRed);
+	gr_c12c12->SetLineStyle(kDashed);
+	gr_c12c13->SetLineStyle(kDashed);
+	gr_c13c13->SetLineStyle(kDashed);
 
 	gr_c12c12->SetLineWidth(2);
 	gr_c12c13->SetLineWidth(2);
@@ -433,11 +456,13 @@ void plot_spp_n(TLegend *leg) {
 	gr_c12c12_n->SetLineColor(kRed);
 	gr_c12c13_n->SetLineColor(kRed);
 	gr_c13c13_n->SetLineColor(kRed);
+	gr_c12c12_n->SetLineStyle(kDashed);
+	gr_c12c13_n->SetLineStyle(kDashed);
+	gr_c13c13_n->SetLineStyle(kDashed);
 
 	gr_c12c12_n->SetLineWidth(2);
 	gr_c12c13_n->SetLineWidth(2);
 	gr_c13c13_n->SetLineWidth(2);
-
 
 	gr_c12c12_n->Draw("C");
 	gr_c12c13_n->Draw("C");
@@ -451,8 +476,8 @@ void plot_tdhf(TLegend *leg) {
 	TGraph* gr_c12c13 = new TGraph("tdhf/x_12C_13C.out","%lg %lg");
 	convert_xsec_2_s(gr_c12c13,12*13/25.,1e-3);
 
-	gr_c12c12->SetLineColor(kGreen+2);
-	gr_c12c13->SetLineColor(kGreen+2);
+	gr_c12c12->SetLineColor(kBlue+1);
+	gr_c12c13->SetLineColor(kBlue+1);
 
 	gr_c12c12->SetLineWidth(2);
 	gr_c12c13->SetLineWidth(2);
@@ -468,8 +493,8 @@ void plot_tdhf_n(TLegend *leg) {
 	TGraph* gr_c12c13_n = new TGraph("tdhf/x_12C_13C_3mev.out","%lg %lg");
 	convert_xsec_2_s(gr_c12c13_n,12*13/25.,1e-3 * 0.67);
 
-	gr_c12c12_n->SetLineColor(kGreen+2);
-	gr_c12c13_n->SetLineColor(kGreen+2);
+	gr_c12c12_n->SetLineColor(kBlue+2);
+	gr_c12c13_n->SetLineColor(kBlue+2);
 
 	gr_c12c12_n->SetLineWidth(2);
 	gr_c12c13_n->SetLineWidth(2);
@@ -481,7 +506,8 @@ void plot_tdhf_n(TLegend *leg) {
 }
 
 void plot_esw(TLegend *leg) {
-	TGraph* gr_c12c12 = new TGraph("esw/c12c12_esw_fowler.xsec","%lg %lg");
+	//TGraph* gr_c12c12 = new TGraph("esw/c12c12_esw_fowler.xsec","%lg %lg");
+	TGraph* gr_c12c12 = new TGraph("esw/esw_uf.xsec","%lg %lg");
 	convert_xsec_2_s(gr_c12c12,6.0,1.);
 	TGraph* gr_c12c13 = new TGraph("esw/c12c13_esw_dayras.xsec","%lg %lg");
 	convert_xsec_2_s(gr_c12c13,12*13/25.,1.);
@@ -495,22 +521,36 @@ void plot_esw(TLegend *leg) {
 	gr_c12c12->SetLineWidth(2);
 	gr_c12c13->SetLineWidth(2);
 	gr_c13c13->SetLineWidth(2);
+	//gr_c12c13->SetLineStyle(9);
 
 
-//	gr_c12c12->Draw("C");
+	//gr_c12c12->Draw("C");
 	gr_c12c13->Draw("C");
 //	gr_c13c13->Draw("C");
-	leg->AddEntry(gr_c12c12, "ESW", "L");
+	leg->AddEntry(gr_c12c13, "ESW", "L");
+	//leg->AddEntry(gr_c12c12, "^{12}C+^{12}C(ESW)", "L");
+}
+
+void plot_esw_uf(TLegend *leg) {
+	TGraph* gr_c12c12 = new TGraph("esw/esw_uf.xsec","%lg %lg");
+	convert_xsec_2_s(gr_c12c12,6.0,1.);
+
+	gr_c12c12->SetLineColor(kBlack);
+
+	gr_c12c12->SetLineWidth(2);
+
+	gr_c12c12->Draw("C");
+	//leg->AddEntry(gr_c12c12, "^{12}C+^{12}C(ESW)", "L");
 }
 void plot_kns(TLegend *leg) {
 	TGraph* gr_c12c12_kns = new TGraph("kns.smod","%lg %lg");
 	convert_smod_2_s(gr_c12c12_kns, 12*12/24.);
-	gr_c12c12_kns->SetLineColor(kBlue);
+	gr_c12c12_kns->SetLineColor(kBlack);
 	gr_c12c12_kns->SetLineWidth(2);
-	gr_c12c12_kns->SetLineStyle(kDashed);
+	gr_c12c12_kns->SetLineStyle(5);
 
 	gr_c12c12_kns->Draw("C");
-	leg->AddEntry(gr_c12c12_kns, "KNS", "L");
+	leg->AddEntry(gr_c12c12_kns, "^{12}C+^{12}C(KNS)", "L");
 }
 
 void Fit_low_E(TMultiGraph* mg_temp, TF1* func, Double_t mu, Double_t xmin, Double_t xmax) {
